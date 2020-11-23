b1961c13ba3b7ededb066b3a092827ac98b20d44
Vladislav <exs2199@gmail.com>
2020-11-23 20:10:02 +0200

    init app book_finder
    This app will search through file system to find books

--- library/library/settings.py
+++ library/library/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'book_finder.apps.BookFinderConfig',
 ]
 
 MIDDLEWARE = [
