bcf4a94a89877219a5f98915f7a56347ee5e671d
Vladislav <exs2199@gmail.com>
2020-11-25 23:02:23 +0200

    update: test: Add check for folder model
    Also, update book`s tests to include new model (Folder)

--- library/books/tests/test_book_author_model.py
+++ library/books/tests/test_book_author_model.py
@@ -1,6 +1,22 @@
 from django.test import TestCase
 
-from books.models import Author, Book
+from books.models import Author, Book, Folder
+
+
+class FolderTest(TestCase):
+
+    def test_folder_create(self):
+        folder = Folder.objects.create(name='First', size=9.5, created='2020-11-25')
+        self.assertEquals(folder.name, 'First')
+
+    def test_subfolder_create(self):
+        main_parent_folder = Folder.objects.create(name='LocalLibraryBaseDir', size=1, created='2020-11-25')
+        folder = Folder.objects.create(name='Main', size=1, created='2020-11-25', parent_folder=main_parent_folder)
+        subfolder1 = Folder.objects.create(name='First', size=9.5, created='2020-11-25', parent_folder=folder)
+        subfolder2 = Folder.objects.create(name='Second', size=1.5, created='2020-11-25', parent_folder=folder)
+
+        self.assertEquals(len(folder.subfolders.all()), 2)
+        self.assertEquals(subfolder2.parent_folder.name, 'Main')
 
 
 class AuthorTest(TestCase):
@@ -16,13 +32,14 @@ class BookTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         Author.objects.create(name='Thomas', surname='Pikkety')
+        Folder.objects.create(name='LocalLibraryBaseDir', size=1, created='2020-11-25')
 
     def test_book_create(self):
         book = Book(title='Capital in the Twenty-First Century',
                     size=10, path='SomeFolder/AnotherFolder/Book.fb2',
                     time_of_adding_to_system='2020-11-24',
                     rate=5, have_read=True, is_favorite=True,
-                    review='Very interesting')
+                    review='Very interesting', folder=Folder.objects.get(pk=1))
         book.save()
         self.assertIsInstance(book, Book)
         self.assertEquals(book.title, 'Capital in the Twenty-First Century')
@@ -33,7 +50,7 @@ class BookTest(TestCase):
                     size=10, path='SomeFolder/AnotherFolder/Book.fb2',
                     time_of_adding_to_system='2020-11-24',
                     rate=5, have_read=True, is_favorite=True,
-                    review='Very interesting')
+                    review='Very interesting', folder=Folder.objects.get(pk=1))
         book.save()
         book.author.add(author)
         book.save()
@@ -41,3 +58,4 @@ class BookTest(TestCase):
         self.assertIsInstance(related_author, Author)
         self.assertEquals(related_author.name, 'Thomas')
         self.assertIsInstance(related_author.books.all()[0], Book)
+        self.assertIsInstance(book.folder, Folder)
