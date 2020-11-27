6d7850c0b7a2292fb2f5a306632471218aa3cbc9
Vladislav <exs2199@gmail.com>
2020-11-27 23:37:27 +0200

    logic: Add logic to save found structure to db

---
+++ library/books/services/save_data_to_db.py
@@ -0,0 +1,26 @@
+from books.models import Folder, Author, Book
+
+from book_finder.services.finder import Directory
+
+
+class Saver:
+    def save_structure_to_db(self, directory: Directory, parent_folder: Folder = None) -> dict:
+        new_folder = Folder.objects.create(name=directory.name,
+                                           parent_folder=parent_folder,
+                                           size=directory.size,
+                                           created=directory.file_creation_time)
+        if directory.includes:  # if directory contains book - save them
+            self._save_book_instances_to_db(new_folder, directory.includes)
+
+        if directory.subdir:  # if directory contains subdirs - save them
+            for subdir in directory.subdir:
+                self.save_structure_to_db(subdir, parent_folder=new_folder)
+
+        return {'status': 'OK'}
+
+    @staticmethod
+    def _save_book_instances_to_db(parent_folder: Folder, books: list):
+        for book in books:
+            Book.objects.create(title=book.name, size=book.size,
+                                path=book.path, file_creation_time=book.file_creation_time,
+                                rate=1, folder=parent_folder)
---
+++ library/books/tests/test_saver.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+
+from book_finder.services.finder import Finder
+
+from books.services.save_data_to_db import Saver
+
+
+class SaverTest(TestCase):
+    path = 'D:\\Personal\\Books\\Math'
+
+    @classmethod
+    def setUpTestData(cls):
+        finder = Finder()
+        cls.folder = finder.find_books_in_system(cls.path)
+
+    def test_saver(self):
+        saver = Saver()
+        status = saver.save_structure_to_db(self.folder)
+        self.assertEquals(status['status'], 'OK')
