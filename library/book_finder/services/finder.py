c890f950804bf1ab2d0958761d13be4c7146dc4c
Vladislav <exs2199@gmail.com>
2020-11-27 21:36:05 +0200

    update: finder.py: Get file creation time for books

--- library/book_finder/services/finder.py
+++ library/book_finder/services/finder.py
@@ -1,6 +1,7 @@
 from typing import Optional, List
 from dataclasses import dataclass, field
 from functools import reduce
+from datetime import datetime
 import os
 
 
@@ -14,7 +15,18 @@ class BaseItem:
 @dataclass
 class Book(BaseItem):
     extension: str  # format: pdf, fb2, epub, etc
+    file_creation_time: str = None
 
+    def __post_init__(self):
+        self.file_creation_time = self._get_file_creation_time()
+
+    def _get_file_creation_time(self):
+        """Transform unix type time into common.
+        Not a part of the public API
+        """
+
+        unix_time = os.path.getctime(self.path)
+        return datetime.utcfromtimestamp(unix_time).strftime('%Y-%m-%d %H:%M:%S')
 
 @dataclass
 class Folder(BaseItem):
@@ -47,8 +59,10 @@ class Finder:
                     book = Book(name=item_name, path=item_path,
                                 extension=item_extension, size=os.path.getsize(item_path) / 1000000)
                     folder.includes.append(book)
+
                 elif os.path.isdir(item_path) and not item.startswith('.'):
                     folder.subfolders.append(self.find_books_in_system(item_path, item))
+
                 else:
                     continue
         folder.size_counter()
