2df75d94aa8572cd3fac9f02c75885685d75e045
Vladislav <exs2199@gmail.com>
2020-11-30 22:47:59 +0200

    logic: Add view to list all folders and books
    Also add urls.py

--- library/books/views.py
+++ library/books/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from .models import Book, Folder
+
+
+class ListBooksView(View):
+    template = 'books/list.html'
+
+    def get(self, request, subdir_id=None):
+        if subdir_id:
+            folder = get_object_or_404(Folder, id=subdir_id)
+        else:
+            folder = get_object_or_404(Folder, is_top_folder=True)
+        subdirs = folder.subfolders.get_queryset().all()
+        books = folder.books.get_queryset().all()
+
+        return render(request, self.template, {'folder': folder,
+                                               'subdirs': subdirs,
+                                               'books': books})
