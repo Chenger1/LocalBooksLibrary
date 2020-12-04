08223770c230ea8ffca3bc7af87ccac4a6821c18
Vladislav <exs2199@gmail.com>
2020-12-04 21:30:04 +0200

    update: ListBooksView change argument
    Now he called 'dir_id'

--- library/books/views.py
+++ library/books/views.py
@@ -2,17 +2,18 @@ from django.shortcuts import render
 from django.views.generic import View
 from django.shortcuts import get_object_or_404
 
-from .models import Book, Folder
+from .models import Folder
 
 
 class ListBooksView(View):
     template = 'books/list.html'
 
-    def get(self, request, subdir_id=None):
-        if subdir_id:
-            folder = get_object_or_404(Folder, id=subdir_id)
+    def get(self, request, dir_id=None):
+        if dir_id:
+            folder = get_object_or_404(Folder, id=dir_id)
         else:
             folder = get_object_or_404(Folder, is_top_folder=True)
+
         subdirs = folder.subfolders.get_queryset().all()
         books = folder.books.get_queryset().all()
 
