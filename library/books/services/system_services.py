af29a0058033285006130aa4b73e5b05aa2e6026
Vladislav <exs2199@gmail.com>
2020-12-11 20:50:06 +0200

    logic: Func to write data and check folder
    write_data_to_file
    checks_books_folder_last_update - open .json file and compare last saved update time and current

--- library/books/services/system_services.py
+++ library/books/services/system_services.py
@@ -3,6 +3,8 @@ import os
 
 from django.conf import settings
 
+from common.system_data import transform_unix_time
+
 
 def init_system_data_file() -> str:
     """
@@ -18,3 +20,26 @@ def init_system_data_file() -> str:
             json.dump([], file)
 
     return path
+
+
+def write_data_to_file(data: dict, file: str):
+    with open(file, 'w', encoding='utf-8') as file:
+        json.dump(data, file)
+
+
+def check_books_folder_last_update(file_path: str) -> bool:
+    """
+        Check last book`s folder update time and compares it with saved in json data.
+        If it are not equal: return False. Else: return True
+
+    :param file_path:
+    :return: bool
+    """
+    with open(file_path, 'r', encoding='utf-8') as file:
+        info = json.load(file)
+
+    last_time_update = transform_unix_time(os.path.getmtime(info['folder_path']))
+
+    if last_time_update != info['last_time_update']:
+        return False
+    return True
