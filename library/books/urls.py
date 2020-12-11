cf317084fc6b062e4b32cce9399da82fecd025cf
Vladislav <exs2199@gmail.com>
2020-12-11 22:49:46 +0200

    logic: Add view to check book`s folder for update
    If folders have been update, updates structure in db

--- library/books/urls.py
+++ library/books/urls.py
@@ -9,4 +9,5 @@ app_name = 'books'
 urlpatterns = [
     path('', views.ListBooksView.as_view(), name='list_top_folder'),
     path('<int:dir_id>', views.ListBooksView.as_view(), name='list_books'),
+    path('check_folder_for_update/', views.CheckFoldersUpdate.as_view(), name='check_folder_for_update'),
 ]
--- library/books/views.py
+++ library/books/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import View
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import HttpResponse
 
 from .models import Folder
 
 from books.utils.structure_manager import StructureManager
 from books.utils.subfolders_utils import get_folders
+from books.services.system_services import init_system_data_file, check_books_folder_last_update, get_data_from_file
+from books.services.save_data_to_db import Saver
+
+from book_finder.services.finder import Finder
 
 
 class ListBooksView(View):
@@ -33,3 +38,14 @@ class ListBooksView(View):
                                                'parent_folder': parent_folder,
                                                'next_folder': next_folder
                                                })
+
+
+class CheckFoldersUpdate(View):
+    def get(self, request):
+        file = init_system_data_file()
+        if check_books_folder_last_update(file):
+            folder_path = get_data_from_file()
+            data = Finder.find_books_in_system(folder_path)
+            Saver.save_structure_to_db(data)
+
+        return redirect('books:list_top_folder')
