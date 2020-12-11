3d94347ccac567ff870791cd3ffc2888881b3290
Vladislav <exs2199@gmail.com>
2020-12-11 20:07:46 +0200

    update:init_system_dat_file returns path

--- library/books/services/system_services.py
+++ library/books/services/system_services.py
@@ -4,14 +4,17 @@ import os
 from django.conf import settings
 
 
-def init_system_data_file():
+def init_system_data_file() -> str:
     """
     system_data file contains information about last book`s folder update.
 
         Firstly check if file exists, if not: creates it
+        :returns folder`s path
     """
 
     path = f'{settings.BASE_DIR}\\system_data.json'
     if not os.path.isfile(path):
         with open(path, 'w', encoding='utf-8') as file:
             json.dump([], file)
+
+    return path
