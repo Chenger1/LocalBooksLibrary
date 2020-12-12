80afc0b56a48a3dfce4c8d8e4e652729a574b781
Vladislav <exs2199@gmail.com>
2020-12-12 20:49:08 +0200

    update: Add classmethod decorator to Saver methods

--- library/books/services/save_data_to_db.py
+++ library/books/services/save_data_to_db.py
@@ -4,19 +4,20 @@ from book_finder.services.finder import Directory
 
 
 class Saver:
-    def save_structure_to_db(self, directory: Directory, parent_folder: Folder = None) -> dict:
+
+    @classmethod
+    def save_structure_to_db(cls, directory: Directory, parent_folder: Folder = None) -> dict:
         new_folder = Folder.objects.create(name=directory.name,
                                            parent_folder=parent_folder,
                                            size=directory.size,
                                            created=directory.file_creation_time,
                                            is_top_folder=False if parent_folder else True)
         if directory.includes:  # if directory contains book - save them
-            self._save_book_instances_to_db(new_folder, directory.includes)
+            cls._save_book_instances_to_db(new_folder, directory.includes)
 
         if directory.subdir:  # if directory contains subdirs - save them
             for subdir in directory.subdir:
-                self.save_structure_to_db(subdir, parent_folder=new_folder)
-
+                cls.save_structure_to_db(subdir, parent_folder=new_folder)
         return {'status': 200}
 
     @staticmethod
