4aee10cb3fac09939b02924256f3126e3e96bbc3
Vladislav <exs2199@gmail.com>
2020-12-15 22:25:39 +0200

    update: change date saving. Now is saved as utc

--- library/common/system_data.py
+++ library/common/system_data.py
@@ -1,11 +1,18 @@
 from datetime import datetime
+import pytz
+
+utc = pytz.UTC
 
 
-def transform_unix_time(unix_time: float, full_time: bool) -> str:
+def transform_unix_time(unix_time: float) -> datetime:
     """
     Transform unix type time into common
-    :param full_time: If true: func returns '%Y-%m-%d %H:%M:%S' format, else '%Y-%m-%d'
     :param unix_time: usual unix format time
-    :return: %Y-%m-%d
+    :return: datetime
     """
-    return datetime.utcfromtimestamp(unix_time).strftime('%Y-%m-%d %H:%M:%S' if full_time else '%Y-%m-%d')
+    return transform_to_utc(datetime.utcfromtimestamp(unix_time))
+
+
+def transform_to_utc(naive_datetime: datetime) -> datetime:
+    return naive_datetime.replace(tzinfo=utc)
+
