3ba0fc368170aad8dad75c4441a7f1247eb9949f
Vladislav <exs2199@gmail.com>
2020-12-15 18:29:15 +0200

    update: get_folders to use it for return None if folder doesnt exist

--- library/books/utils/subfolders_utils.py
+++ library/books/utils/subfolders_utils.py
@@ -5,10 +5,15 @@ from books.models import Folder
 from django.core.exceptions import ObjectDoesNotExist
 
 
-def get_folders(folder_id: int) -> Union[int, None]:
+def get_folders(folder_id: int = None, is_top_folder: bool = False) -> Union[Folder, None]:
     try:
-        folder = Folder.objects.get(pk=folder_id)
+        if folder_id:
+            folder = Folder.objects.get(pk=folder_id)
+        elif is_top_folder:
+            folder = Folder.objects.get(is_top_folder=True)
+        else:
+            return None
     except ObjectDoesNotExist:
-        folder = None
-
-    return folder
+        return None
+    else:
+        return folder
