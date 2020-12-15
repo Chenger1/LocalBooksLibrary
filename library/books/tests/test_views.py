2f9b351fd8ea8fca10b6a280a9afe9c2c7227539
Vladislav <exs2199@gmail.com>
2020-12-15 19:55:34 +0200

    test: test for post view for saving data

--- library/books/tests/test_views.py
+++ library/books/tests/test_views.py
@@ -4,5 +4,10 @@ from django.test import TestCase
 class CheckFoldersUpdateTest(TestCase):
 
     def test_view_url_exists_at_desired_location(self):
-        resp = self.client.get('/check_folder_for_update/')
+        resp = self.client.get('/check_folder_for_update/', follow=True)
+        self.assertEqual(resp.status_code, 200, msg=resp.content)
+
+    def test_save_data_url_pattern(self):
+        folder_path = 'D://Personal/Books'
+        resp = self.client.post('/save_data/', {'folder_path': folder_path}, follow=True)
         self.assertEqual(resp.status_code, 200, msg=resp.content)
