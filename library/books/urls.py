fdc7733dfe3927c65af21c7bc335513093488e3e
Vladislav <exs2199@gmail.com>
2020-12-15 19:29:45 +0200

    update: save_data url pattern for saving data to db

--- library/books/urls.py
+++ library/books/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('', views.ListBooksView.as_view(), name='list_top_folder'),
     path('<int:dir_id>', views.ListBooksView.as_view(), name='list_books'),
     path('check_folder_for_update/', views.CheckFoldersUpdate.as_view(), name='check_folder_for_update'),
+    path('save_data/', views.CheckFoldersUpdate.as_view(),
+         name='save_data'),
 ]
