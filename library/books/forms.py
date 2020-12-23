9baf800b8165c589b6f57c04ce290928a2159407
Vladislav <exs2199@gmail.com>
2020-12-23 14:18:11 +0200

    update: delete an extra field from form

--- library/books/forms.py
+++ library/books/forms.py
@@ -4,5 +4,4 @@ from books.models import Folder
 
 
 class AddNewBookForm(forms.Form):
-    book_file_path = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
     folders = forms.ModelChoiceField(queryset=Folder.objects.all(), empty_label='Nothing')
