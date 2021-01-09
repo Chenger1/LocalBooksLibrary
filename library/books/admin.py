6313399e04bad8683f7941915092327993256f75
Vladislav <exs2199@gmail.com>
2021-01-09 22:48:42 +0200

    logic: add Author model to admin page

--- library/books/admin.py
+++ library/books/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Book, Folder
+from .models import Book, Folder, Author
 
 
 @admin.register(Folder)
@@ -12,3 +12,8 @@ class FolderAdmin(admin.ModelAdmin):
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ['title']
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'surname']
