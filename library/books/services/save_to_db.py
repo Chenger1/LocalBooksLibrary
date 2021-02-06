b6007ea3a89d12f4d285422b4434f9cd77665274
Vladislav <exs2199@gmail.com>
2021-02-06 23:19:49 +0200

    Update: Move book_info methods from Ebooks saver to Books saver according to new models

--- library/books/services/save_to_db.py
+++ library/books/services/save_to_db.py
@@ -1,4 +1,4 @@
-from typing import  Dict
+from typing import Dict
 
 from books.models import Book, Author
 
@@ -12,3 +12,19 @@ class Saver:
                                         genre=data.get('genre'),
                                         rate=1)
         return book_inst
+
+    @classmethod
+    def add_author_to_db(cls, author_data: Dict[str, str]) -> Author:
+        author_ins, is_created = Author.objects.get_or_create(name=author_data['name'], surname=author_data['surname'])
+        return author_ins
+
+    @classmethod
+    def update_book_info(cls, book: Book, info_to_update: Dict[str, str]):
+        author_data = {}
+        temp = info_to_update['author'].split(' ')
+        author_data['name'], author_data['surname'] = temp[0], temp[1]
+        author = cls.add_author_to_db(author_data)
+        book.genre = info_to_update['genre']
+        book.description = info_to_update['annotation']
+        book.author.add(author)
+        book.save()
