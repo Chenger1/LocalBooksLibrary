5c393d2d109c502ff1590ca18c7d407f2af3de7b
Vladislav <exs2199@gmail.com>
2021-02-07 23:28:02 +0200

    Update: Add models to admin page

--- library/books/admin.py
+++ library/books/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from books.models import Book, Author
+from ebooks.models import Ebook
+
+
+class EbookInline(admin.StackedInline):
+    model = Ebook
+    extra = 0
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'genre', 'rate']
+    list_filter = ['author__surname', 'genre', 'rate', 'have_read', 'to_read', 'is_favorite']
+    inlines = [EbookInline]
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'surname']
+    list_filter = ['name', 'surname']
