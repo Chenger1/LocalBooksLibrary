25d6903cf954a79945f3f2e520a080c381dba728
Vladislav <exs2199@gmail.com>
2021-02-08 13:40:30 +0200

    Logic: Add filtering by 'is_favorite' attribute

--- library/books/urls.py
+++ library/books/urls.py
@@ -6,4 +6,5 @@ app_name = 'books'
 
 urlpatterns = [
     path('', views.ListBookView.as_view(), name='list_books'),
+    path('is_favorite/', views.ListFavoriteBookView.as_view(), name='list_favorite_books'),
 ]
--- library/books/views.py
+++ library/books/views.py
@@ -6,3 +6,8 @@ from books.models import Book
 class ListBookView(ListView):
     model = Book
     template_name = 'book/list_book.html'
+
+
+class ListFavoriteBookView(ListBookView):
+    def get_queryset(self):
+        return self.model.objects.filter(is_favorite=True)
